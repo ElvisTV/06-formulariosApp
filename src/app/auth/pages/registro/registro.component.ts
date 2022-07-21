9567ab93452e806c6b899a1b303521b8ac9c7de0
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-07-21 09:10:55 -0500

    funcion para validar campos iguaes

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from '../../../shared/validator/validaciones';
+import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -9,16 +11,26 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
 
-  // TO DO: Temporal
-  nombreApellidoPattern: string = '([a-zA-A]+) ([a-zA-Z]+)';
-
   miFormulario: FormGroup  = this.fb.group( {
-    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ] ]
+    nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern ) ] ],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ] ],
+    username: ['', [Validators.required , this.validatorService.noPuedeSerStrider ]],
+    password: ['', [Validators.required, Validators.minLength(6) ] ],
+    password2: ['',[ Validators.required  ] ]
+  }, {
+    validators: [ this.validatorService.camposIguales('password','password2') ]
   } )
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private validatorService: ValidatorService ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Fernando Herrera',
+      email: 'elvistezen@outlook.es',
+      username: 'elvisTV'
+    })
+
   }
 
   campoNoValido ( campo: string ) {
